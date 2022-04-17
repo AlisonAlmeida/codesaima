3c01b7bb4d298ccc8388b13eb6198de7c389ca0f
AlisonAlmeida <alison.asa@gmail.com>
2022-04-17 15:37:36 -0400

    new

--- lib/screens/searchs/morar_melhor_search_screen.dart
+++ lib/screens/searchs/morar_melhor_search_screen.dart
@@ -7,7 +7,7 @@ class MorarMelhorSearchScreen extends StatelessWidget {
   MorarMelhorSearchScreen({Key? key}) : super(key: key);
   final String name = 'Morar Melhor';
   final _form = GlobalKey<FormState>();
-  final _valor = TextEditingController();
+  final _nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +16,45 @@ class MorarMelhorSearchScreen extends StatelessWidget {
         title: Text(name),
         backgroundColor: Colors.orange,
       ),
-      body: Form(
-        key: _form,
-        child: TextField(controller: _valor),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                    labelText: 'Nome',
+                    hintText: 'Nome Completo',
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Icon(Icons.person),
+                    )),
+              ),
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                    labelText: 'CEP',
+                    hintText: 'CEP',
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Icon(Icons.numbers),
+                    )),
+              ),
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                    labelText: 'Rua',
+                    hintText: 'Rua sem o número',
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Icon(Icons.add_road),
+                    )),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
