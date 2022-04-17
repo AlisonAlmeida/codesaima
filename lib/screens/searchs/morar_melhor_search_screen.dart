48fd8f26de2db4ac8eb7f42183904caafcbca4dc
AlisonAlmeida <alison.asa@gmail.com>
2022-04-17 03:15:21 -0400

    ç

--- lib/screens/searchs/morar_melhor_search_screen.dart
+++ lib/screens/searchs/morar_melhor_search_screen.dart
@@ -1,10 +1,13 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, unused_field
 
+import 'package:codesaima/consts.dart';
 import 'package:flutter/material.dart';
 
 class MorarMelhorSearchScreen extends StatelessWidget {
   MorarMelhorSearchScreen({Key? key}) : super(key: key);
   final String name = 'Morar Melhor';
+  final _form = GlobalKey<FormState>();
+  final _valor = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +15,10 @@ class MorarMelhorSearchScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(name),
         backgroundColor: Colors.orange,
+      ),
+      body: Form(
+        key: _form,
+        child: TextField(controller: _valor),
       ),
     );
   }
